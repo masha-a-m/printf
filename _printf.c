6b2f47009c764bc404c4bb6c1b61d28a7d7a7ede
masha-a-m <escritoramaree@gmail.com>
2023-01-25 14:47:30 -0800

    print

--- _printf.c
+++ _printf.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 void print_buffer(char buffer[], int *buff_ind);
+
 /**
  * _printf - Printf function
  * @format: format.
@@ -12,9 +13,12 @@ int i, printed = 0, printed_chars = 0;
 int flags, width, precision, size, buff_ind = 0;
 va_list list;
 char buffer[BUFF_SIZE];
+
 if (format == NULL)
 return (-1);
+
 va_start(list, format);
+
 for (i = 0; format && format[i] != '\0'; i++)
 {
 if (format[i] != '%')
@@ -41,9 +45,12 @@ printed_chars += printed;
 }
 }
 print_buffer(buffer, &buff_ind);
+
 va_end(list);
+
 return (printed_chars);
 }
+
 /**
  * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
@@ -53,5 +60,6 @@ void print_buffer(char buffer[], int *buff_ind)
 {
 if (*buff_ind > 0)
 write(1, &buffer[0], *buff_ind);
+
 *buff_ind = 0;
 }
